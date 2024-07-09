dfa34c8e6add71b58f178ac91628b32bc8649995
KarimMalki <145053673+KarimMalki@users.noreply.github.com>
2024-07-09 19:36:43 -0400

    Made signup a blank screen

---
+++ src/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css';
+
+const BlankScreen = () => {
+    return (
+        <div className="blank-screen">
+            {/* This div will take up the full screen and be blank white */}
+        </div>
+    );
+};
+
+export default BlankScreen;
